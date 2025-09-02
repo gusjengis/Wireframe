45fa3c3b9f39fe0e1d182d1752b81dd6b8aa41fe
gusjengis <anthony.j.green@outlook.com>
2025-09-01 20:55:14 -0700

    fixed issues with clicking when page is scaled

--- canvas.js
+++ canvas.js
@@ -2,56 +2,56 @@
 
 var canvas = document.getElementById('canvas');
 var viewportDraw = canvas.getContext('2d');
-var bufferImage =  document.createElement('canvas');
+var bufferImage = document.createElement('canvas');
 var draw = bufferImage.getContext('2d');
 var glDraw = bufferImage.getContext('webgl');
 const BufferHeight = 240;
 // draw.translate(0.5, 0.5);
 
 var backgroundColor = "rgba(0,0,0,1)";
- 
-function canvasOnLoad(){
-    generateBufferImage();
-    canvasResize();
+
+function canvasOnLoad() {
+        generateBufferImage();
+        canvasResize();
 }
 
-function canvasResize(){
-    // document.body.style.zoom=window.devicePixelRatio;
-    document.body.style.zoom= 1/window.devicePixelRatio;
-    canvas.width = window.devicePixelRatio*document.documentElement.clientWidth;
-    canvas.height = window.devicePixelRatio*document.documentElement.clientHeight;
-    bufferImage.width = canvas.width/(canvas.height/BufferHeight);
-    applyBufferImage();
+function canvasResize() {
+        // document.body.style.zoom=window.devicePixelRatio;
+        document.body.style.zoom = 1 / window.devicePixelRatio;
+        canvas.width = window.devicePixelRatio * document.documentElement.clientWidth;
+        canvas.height = window.devicePixelRatio * document.documentElement.clientHeight;
+        bufferImage.width = canvas.width / (canvas.height / BufferHeight);
+        applyBufferImage();
 }
 
-function generateBufferImage(){
-    
-    bufferImage.width = canvas.width/(canvas.height/BufferHeight);
-    bufferImage.height = BufferHeight;
-    var height = bufferImage.height;
-    var width = bufferImage.width;
-    for(x = 0; x < width; x++){
-        for(y = 0; y < height; y++){
+function generateBufferImage() {
 
-            var r = 255*x/width;
+        bufferImage.width = canvas.width / (canvas.height / BufferHeight);
+        bufferImage.height = BufferHeight;
+        var height = bufferImage.height;
+        var width = bufferImage.width;
+        for (x = 0; x < width; x++) {
+                for (y = 0; y < height; y++) {
 
-            var g = 255*(y)/height;
+                        var r = 255 * x / width;
 
-            var b = 100*(width-x)/width;
+                        var g = 255 * (y) / height;
 
-            draw.fillStyle = 'rgb('+r+','+g+','+b+')';
+                        var b = 100 * (width - x) / width;
 
-            draw.fillRect(x,y,1,1);
+                        draw.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+
+                        draw.fillRect(x, y, 1, 1);
+                }
         }
-    }
 }
 
-function applyBufferImage(){
-    viewportDraw.imageSmoothingEnabled = false;
-    viewportDraw.drawImage(bufferImage, 0, 0, canvas.width, canvas.height);
+function applyBufferImage() {
+        viewportDraw.imageSmoothingEnabled = false;
+        viewportDraw.drawImage(bufferImage, 0, 0, canvas.width, canvas.height);
 }
 
-function clear(){
-    draw.fillStyle = backgroundColor;
-    draw.fillRect(0,0,bufferImage.width, bufferImage.height);
+function clear() {
+        draw.fillStyle = backgroundColor;
+        draw.fillRect(0, 0, bufferImage.width, bufferImage.height);
 }
--- main.js
+++ main.js
@@ -1,70 +1,70 @@
 class Point {
-    constructor(x,y){
+    constructor(x, y) {
         this.x = x;
         this.y = y;
     }
 }
 
 class Line {
-    constructor(point1, point2){
+    constructor(point1, point2) {
         this.point1 = point1;
         this.point2 = point2;
-        this.length = ((point2.x-point1.x)**2+(point2.y-point1.y)**2)**(1/2);
+        this.length = ((point2.x - point1.x) ** 2 + (point2.y - point1.y) ** 2) ** (1 / 2);
     }
 
-    draw(){
+    draw() {
         draw.fillStyle = "#a12c2c";
         line(this.point1.x, this.point1.y, this.point2.x, this.point2.y);
     }
 }
 
 class Wireframe {
-    constructor(){
+    constructor() {
         this.points = [];
         this.lines = [];
     }
 
-    addPoint(x,y){
-        this.points.push(new Point(x,y));
+    addPoint(x, y) {
+        this.points.push(new Point(x, y));
         this.lines = [];
         let j;
         let k;
-        for(j=0; j<this.points.length; j++){
-            for(k=0; k<this.points.length; k++){
+        for (j = 0; j < this.points.length; j++) {
+            for (k = 0; k < this.points.length; k++) {
                 this.lines.push(new Line(this.points[j], this.points[k]));
             }
         }
     }
 
-    centerX(){
+    centerX() {
         let sum = 0;
-        for(i=0; i<this.points.length; i++){
+        for (i = 0; i < this.points.length; i++) {
             sum += this.points[i].x;
         }
         return x;
     }
 
-    centerY(){
+    centerY() {
         let sum = 0;
-        for(i=0; i<this.points.length; i++){
+        for (i = 0; i < this.points.length; i++) {
             sum += this.points[i].y;
         }
         return y;
     }
 
-    draw(){
+    draw() {
         let j;
-        for(j=0; j<this.lines.length; j++){
+        for (j = 0; j < this.lines.length; j++) {
             this.lines[j].draw();
         }
     }
 }
 
-window.onload = function() {
+window.onload = function () {
     canvasOnLoad();
     drawRandomTriangle();
 }
-window.onresize = function(){
+window.onresize = function () {
     canvasResize();
     change = true;
 }
@@ -74,25 +74,27 @@ let y;
 let groundY = 260;
 let change = true;
 
-document.onmousemove = function(e){
-    x = Math.floor(e.clientX/(canvas.width/bufferImage.width));
-    y = Math.floor(e.clientY/(canvas.height/bufferImage.height));
+document.onmousemove = function (e) {
+    x = Math.floor(e.clientX / (canvas.width / bufferImage.width));
+    y = Math.floor(e.clientY / (canvas.height / bufferImage.height));
 }
 
-document.onclick = function(){
-    wireframe.addPoint(x,y);
+document.onclick = function () {
+    let scaled_x = x * window.devicePixelRatio;
+    let scaled_y = y * window.devicePixelRatio;
+    wireframe.addPoint(scaled_x, scaled_y);
     change = true;
 }
 let wireframe = new Wireframe();
 
 
-function drawLoop(timestamp){
-    
-    if(keyboard.SPACE){
+function drawLoop(timestamp) {
+
+    if (keyboard.SPACE) {
         drawRandomTriangle();
     }
 
-    if(change){
+    if (change) {
         clear();
         wireframe.draw();
         applyBufferImage();
@@ -103,35 +105,35 @@ function drawLoop(timestamp){
 
 requestAnimationFrame(drawLoop);
 
-function drawRandomTriangle(){
+function drawRandomTriangle() {
     clear();
     wireframe = new Wireframe();
-    wireframe.addPoint(Math.random()*bufferImage.width, Math.random()*bufferImage.height);
-    wireframe.addPoint(Math.random()*bufferImage.width, Math.random()*bufferImage.height);
-    wireframe.addPoint(Math.random()*bufferImage.width, Math.random()*bufferImage.height);
-    wireframe.addPoint(Math.random()*bufferImage.width, Math.random()*bufferImage.height);
+    wireframe.addPoint(Math.random() * bufferImage.width, Math.random() * bufferImage.height);
+    wireframe.addPoint(Math.random() * bufferImage.width, Math.random() * bufferImage.height);
+    wireframe.addPoint(Math.random() * bufferImage.width, Math.random() * bufferImage.height);
+    wireframe.addPoint(Math.random() * bufferImage.width, Math.random() * bufferImage.height);
     wireframe.draw();
     applyBufferImage();
 }
 
-function line(x1,y1,x2,y2){
-    let deltaX = x2-x1;
-    let deltaY = y2-y1;
-    let magnitude = (deltaX**2 + deltaY**2)**(1/2)
-    let unitX = deltaX/magnitude;
-    let unitY = deltaY/magnitude;
+function line(x1, y1, x2, y2) {
+    let deltaX = x2 - x1;
+    let deltaY = y2 - y1;
+    let magnitude = (deltaX ** 2 + deltaY ** 2) ** (1 / 2)
+    let unitX = deltaX / magnitude;
+    let unitY = deltaY / magnitude;
     let x = x1;
     let y = y1;
-    for(i=0; i<magnitude; i++){
-        rainbowPixel(Math.floor(x),Math.floor(y))
+    for (i = 0; i < magnitude; i++) {
+        rainbowPixel(Math.floor(x), Math.floor(y))
         x += unitX;
         y += unitY;
     }
 }
 
-function rainbowPixel(x,y){
+function rainbowPixel(x, y) {
     let height = bufferImage.height;
     let width = bufferImage.width;
-    draw.fillStyle = "rgb("+255*x/width+","+255*(y)/height+","+150*(width-x)/width+")";
-    draw.fillRect(x,y,1,1);
+    draw.fillStyle = "rgb(" + 255 * x / width + "," + 255 * (y) / height + "," + 150 * (width - x) / width + ")";
+    draw.fillRect(x, y, 1, 1);
 }
